54809d636b9134155c0e13be2db0797f65b7a2b5
AkhoMngqibisa <akhomngqibisa@gmail.com>
2026-09-02 20:20:59 +0200

    feat: add variables example

---
+++ 02-java-syntax/examples/Variables.java
@@ -0,0 +1,15 @@
+public class Variables {
+
+    public static void main(String[] args) {
+
+        String name = "Akhona";
+        int age = 25;
+        double salary = 25000.50;
+        boolean employed = true;
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Salary: " + salary);
+        System.out.println("Employed: " + employed);
+    }
+}

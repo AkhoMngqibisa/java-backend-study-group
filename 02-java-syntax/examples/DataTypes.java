757e04dee3d542869e93aa92279f55a14f01cc42
AkhoMngqibisa <akhomngqibisa@gmail.com>
2026-09-04 18:30:00 +0200

    feat: lesson 2 add example

---
+++ 02-java-syntax/examples/DataTypes.java
@@ -0,0 +1,25 @@
+public class DataTypes {
+
+    public static void main(String[] args) {
+
+        byte smallNumber = 100;
+        short number = 1000;
+        int age = 25;
+        long population = 8000000000L;
+
+        float temperature = 25.5f;
+        double salary = 35000.75;
+
+        char grade = 'A';
+        boolean active = true;
+
+        System.out.println(smallNumber);
+        System.out.println(number);
+        System.out.println(age);
+        System.out.println(population);
+        System.out.println(temperature);
+        System.out.println(salary);
+        System.out.println(grade);
+        System.out.println(active);
+    }
+}

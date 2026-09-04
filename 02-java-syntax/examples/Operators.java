de83934d1f541ae375b81c1e69cb122caa45bb5c
AkhoMngqibisa <akhomngqibisa@gmail.com>
2026-09-04 18:45:00 +0200

    feat: lesson 2 add example

---
+++ 02-java-syntax/examples/Operators.java
@@ -0,0 +1,21 @@
+public class Operators {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 3;
+
+        System.out.println("Addition: " + (a + b));
+        System.out.println("Subtraction: " + (a - b));
+        System.out.println("Multiplication: " + (a * b));
+        System.out.println("Division: " + (a / b));
+        System.out.println("Remainder: " + (a % b));
+
+        System.out.println(a > b);
+        System.out.println(a == b);
+        System.out.println(a != b);
+
+        System.out.println(a > 5 && b < 5);
+        System.out.println(a > 20 || b < 5);
+    }
+}

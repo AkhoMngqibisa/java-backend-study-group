b257f8097c90e8f1ed3b5aa6df7ef610f3ba1732
AkhoMngqibisa <akhomngqibisa@gmail.com>
2026-09-05 18:15:00 +0200

    feat: lesson 3 add example

---
+++ 03-methods-arrays-strings/examples/BasicMethod.java
@@ -0,0 +1,12 @@
+public class BasicMethod {
+
+    public static void sayHello() {
+        System.out.println("Hello from a method!");
+    }
+
+    public static void main(String[] args) {
+
+        sayHello();
+        sayHello();
+    }
+}

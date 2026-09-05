0735573d3fdf82a1674ed781d3a703c5ebc48a19
AkhoMngqibisa <akhomngqibisa@gmail.com>
2026-09-05 18:30:00 +0200

    feat: lesson 3 add example

---
+++ 03-methods-arrays-strings/examples/MethodParameters.java
@@ -0,0 +1,13 @@
+public class MethodParameters {
+
+    public static void greet(String name) {
+
+        System.out.println("Hello " + name);
+    }
+
+    public static void main(String[] args) {
+
+        greet("Akhona");
+        greet("John");
+    }
+}

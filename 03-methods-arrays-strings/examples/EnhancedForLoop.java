ac23a0df3e08184cca3a40f2ec5032ee0ea3a0f2
AkhoMngqibisa <akhomngqibisa@gmail.com>
2026-09-07 18:30:00 +0200

    feat: lesson 3 add example

---
+++ 03-methods-arrays-strings/examples/EnhancedForLoop.java
@@ -0,0 +1,12 @@
+public class EnhancedForLoop {
+
+    public static void main(String[] args) {
+
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        for (int number : numbers) {
+
+            System.out.println(number);
+        }
+    }
+}
